624e7b26939b93f0533a5ea553e3b69ddd6df7fd
MichaelKaffel <michaeldkaffel@gmail.com>
2026-04-06 23:51:08 -0700

    fix: add node globals to server eslint config

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,8 +1,12 @@
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
     js.configs.recommended,
     {
+        languageOptions: {
+            globals: globals.node,
+        },
         rules: {
             'no-unused-vars': 'warn',
             'no-console': 'off',
